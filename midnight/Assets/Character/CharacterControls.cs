777aec8cc3c1ebf7c9449e423eb3958d35709f81
Mortoc <Mortoc@gmail.com>
2013-07-13 23:41:36 -0400

    Implemented the GUI

--- midnight/Assets/Character/CharacterControls.cs
+++ midnight/Assets/Character/CharacterControls.cs
@@ -5,6 +5,15 @@ public abstract class CharacterControls : MonoBehaviour
 {
 	public float CharacterSpeed = 1.0f;
 	
+	protected MainCharacter Character { get; set; }
+	protected SceneGui Gui { get; set; }
+	
+	protected virtual void Start()
+	{
+		Character = Find.ObjectInScene<MainCharacter>();
+		Gui = Find.ObjectInScene<SceneGui>();
+	}
+	
 	
 	// Move the character (negative left, positive right)
 	protected void Translate(float amount)
@@ -12,7 +21,7 @@ public abstract class CharacterControls : MonoBehaviour
 		CharacterController controller = GetComponent<CharacterController>();
 		Vector3 translation = Vector3.right * amount;
 		controller.SimpleMove( translation );
-		
+	
 		PostTranslate(translation);
 	}
 	
--- midnight/Assets/Character/MainCharacter.cs
+++ midnight/Assets/Character/MainCharacter.cs
@@ -3,4 +3,12 @@ using System.Collections.Generic;
 
 public class MainCharacter : MonoBehaviour 
 {
+	public enum Mode { Walking, ObjectPlacement }
+	
+	public Mode CurrentMode { get; set; }
+	
+	void Start()
+	{
+		CurrentMode = Mode.Walking;
+	}
 }
