ce7b2c9729b983ff8d722d6689d5b4e0fb5a9ec8
Chris Mortonson <Mortoc@gmail.com>
2013-12-05 21:17:34 -0500

    reorg

---
+++ midnight/Assets/Prototype/FollowMouse.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour {
+
+	void Update () {
+		transform.position = Camera.main.ScreenPointToRay(Input.mousePosition).GetPoint(9.5f);
+	}
+}
